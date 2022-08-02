75e3b7973b40aeb616eab9f3c5bee8528008ec3a
pujian <pujian@rivamed.cn>
2022-08-02 11:17:58 +0800

    删除临时测试包

--- spring-demo/src/main/java/cn/zefre/spring/mybatisplus/typehandler/BaseInter.java
+++
@@ -1,8 +0,0 @@
-package cn.zefre.spring.mybatisplus.typehandler;
-
-/**
- * @author pujian
- * @date 2022/7/7 17:20
- */
-public interface BaseInter<U> {
-}
--- spring-demo/src/main/java/cn/zefre/spring/mybatisplus/typehandler/MyInterface.java
+++
@@ -1,8 +0,0 @@
-package cn.zefre.spring.mybatisplus.typehandler;
-
-/**
- * @author pujian
- * @date 2022/7/7 17:09
- */
-public interface MyInterface extends GenericEnumType<String>, BaseInter<Integer> {
-}
